082513124e983dee58dc11995dc09689d3086b67
Panjilesmana <54707828+Panjilesmana@users.noreply.github.com>
2019-09-12 20:46:54 +0700

    tugas alpro 12-9-2019
    tugas alpro looping,function
    PUTU PANJI LESMANA
    1201180273
    TI-42-11

---
+++ Latihan.py
@@ -0,0 +1,13 @@
+listGPA = [3,2.7,2.5,4]
+def hadiah(GPA):
+    bonus = 500000
+    return hadiah
+
+
+for GPA in listGPA :
+    if GPA > 3 :
+        print ("Anda mendapatkan Hadiah ; Rp", hadiah(GPA)) 
+    else :
+        print ("tidak ada bonus")   
+
+        
